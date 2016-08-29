8d71bbfefc49ca64efddb540e381e34841839f37
Carl Ruiz <carl.45.cr@gmail.com>
2016-08-29 16:49:49 +0200

    reorganization files

--- server/parser/importDataApiInfojobs.js
+++ server/parser/importDataApiInfojobs.js
@@ -3,8 +3,6 @@ var request = require('request');
 var env = require('node-env-file');
 var MongoClient = require('mongodb').MongoClient;
 
-
-
 var bodyParser = require('body-parser');
 
 var url2 = 'mongodb://localhost:27017/offers';
@@ -36,18 +34,15 @@ MongoClient.connect(url2, function(err, db) {
 
 				var objectJSON = JSON.parse(response.body);
 
-				var _cPlusPlus,_java,_javaScript,_visualBasic,
-					_python,_angular,_php,_NET,_C,_cobol,_go,
-					_sql,_mySql,_node,_express,_django,_sQlite,
-					_mongoDb,_couchDB,_redis,_cassandra,_ruby;
+				var ranking={_numOffers:[{name:"C++",num:0},{name:"Java",num:0},{name:"JavaScript",num:0},
+							{name:"C#",num:0},{name:"Python",num:0},{name:"Angular",num:0},
+							{name:"PHP",num:0},{name:"Net",num:0},{name:"C",num:0},
+							{name:"Cobol",num:0},{name:"Go",num:0}]};
 
-				var arrCont =  [contCplusPlus = 0,contJava = 0,
-								contJavaScript = 0,contVisualBasic = 0,contPython = 0,
-								contAngularjs=0,contPHP=0,contNET  = 0,contCsharp = 0,
-								contCobol=0,contGo=0,contSql = 0,contMySql = 0,
-								contNode = 0,contExpress = 0,contDjango = 0,
-								contSqlite=0,contMongodb=0,contCouchDb  = 0,contRedis = 0,
-								contCassandra=0,contRuby=0];
+				var rankingDb={_numOffers:[{name:"SQL",num:0},{name:"MySQL",num:0},{name:"Node",num:0},
+							{name:"Express",num:0},{name:"Django",num:0},{name:"sQlite",num:0},
+							{name:"MongoDb",num:0},{name:"CouchDB",num:0},{name:"Redis",num:0},
+							{name:"Cassandra",num:0},{name:"Ruby",num:0}]};
 
 					for(var index in objectJSON) {
 						for (var subItem in objectJSON[index]) {
@@ -56,94 +51,90 @@ MongoClient.connect(url2, function(err, db) {
 							var arraySkills = paragraphSkills.split(' ');
 							arraySkills.forEach(function(item,i){
 
-								if(item==='C++'){
-									_cPlusPlus = arrCont[0]++;
+								for (var index2 in ranking) {
+									for (var subItem in ranking[index2]){
+
+											if(item==='C++' && ranking[index2][subItem].name ==="C++"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='JAVA' && ranking[index2][subItem].name ==="Java"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='JAVASCRIPT' && ranking[index2][subItem].name ==="JavaScript"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='C#' && ranking[index2][subItem].name ==="C#"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='PYTHON' && ranking[index2][subItem].name ==="Python"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='ANGULAR' || item==='ANGULARJS' && ranking[index2][subItem].name ==="Angular"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='PHP' && ranking[index2][subItem].name ==="PHP"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='NET' && ranking[index2][subItem].name ==="Net"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='C' && ranking[index2][subItem].name ==="C"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='COBOL' && ranking[index2][subItem].name ==="Cobol"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='GO' && ranking[index2][subItem].name ==="Go"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='SQL'&& ranking[index2][subItem].name ==="SQL"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='MYSQL'&& ranking[index2][subItem].name ==="MySQL"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='NODE' && ranking[index2][subItem].name ==="Node"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='EXPRESS' && ranking[index2][subItem].name ==="Express"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='DJANGO' && ranking[index2][subItem].name ==="Django"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='SQLITE' && ranking[index2][subItem].name ==="sQlite"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='MONGODB' && ranking[index2][subItem].name ==="MongoDb"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='COUCHDB' && ranking[index2][subItem].name ==="CouchDB"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='REDIS' && ranking[index2][subItem].name ==="Redis"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='CASSANDRA' && ranking[index2][subItem].name ==="Cassandra"){
+												ranking[index2][subItem].num++;
+											}
+											if(item==='RUBY' && ranking[index2][subItem].name ==="Ruby"){
+												ranking[index2][subItem].num++;
+											}
+									}
 								}
-								if(item==='JAVA'){
-									_java =  arrCont[1]++;
-								}
-								if(item==='JAVASCRIPT'){
-									_javaScript = arrCont[2]++;
-								}
-								if(item==='C#'){
-									_cSharp = arrCont[3]++;
-								}
-								if(item==='PYTHON'){
-									_python = arrCont[4]++;
-								}
-								if(item==='ANGULAR' || item==='ANGULARJS'){
-									_angular = arrCont[5]++;
-								}
-								if(item==='PHP'){
-									_php = arrCont[6]++;
-								}
-								if(item==='NET'){
-									_NET = arrCont[7]++;
-								}
-								if(item==='C'){
-									_C = arrCont[8]++;
-								}
-								if(item==='COBOL'){
-									_cobol = arrCont[9]++;
-								}
-								if(item==='GO'){
-									_go = arrCont[10]++;
-								}
-								if(item==='SQL'){
-									_sql = arrCont[11]++;
-								}
-								if(item==='MYSQL'){
-									_mySql =  arrCont[12]++;
-								}
-								if(item==='NODE'){
-									_node = arrCont[13]++;
-								}
-								if(item==='EXPRESS'){
-									_express = arrCont[14]++;
-								}
-								if(item==='DJANGO'){
-									_django = arrCont[15]++;
-								}
-								if(item==='SQLITE'){
-									_sQlite = arrCont[16]++;
-								}
-								if(item==='MONGODB'){
-									_mongoDb = arrCont[17]++;
-								}
-								if(item==='COUCHDB'){
-									_couchDB = arrCont[18]++;
-								}
-								if(item==='REDIS'){
-									_redis = arrCont[19]++;
-								}
-								if(item==='CASSANDRA'){
-									_cassandra = arrCont[20]++;
-								}
-								if(item==='RUBY'){
-									_ruby = arrCont[21]++;
-								}
+
 							});
 						}
 				}
 
-				var ranking={_numOffers:[{name:"C++",num:_cPlusPlus},{name:"Java",num:_java},{name:"JavaScript",num:_javaScript},
-							{name:"C#",num:_cSharp},{name:"Python",num:_python},{name:"Angular",num:_angular},
-							{name:"PHP",num:_php},{name:"Net",num:_NET},{name:"C",num:_C},
-							{name:"Cobol",num:_cobol},{name:"Go",num:_go}]};
-
-				var rankingDb={_numOffers:[{name:"SQL",num:_sql},{name:"MySQL",num:_mySql},{name:"Node",num:_node},
-							{name:"Express",num:_express},{name:"Django",num:_django},{name:"sQlite",num:_sQlite},
-							{name:"MongoDb",num:_mongoDb},{name:"CouchDB",num:_couchDB},{name:"Redis",num:_redis},
-							{name:"Cassandra",num:_cassandra},{name:"Ruby",num:_ruby}]};
-
 
 					ranking._numOffers = ranking._numOffers.sort(function(obj1,obj2){
 
-							for(var prop in obj1) {
-								prop1=prop;
+							for(var property in obj1) {
+								prop1=property;
 							}
-							for(prop in obj2) {
-								prop2=prop;
+							for(property in obj2) {
+								prop2=property;
 							}
 							return obj2[prop2]-obj1[prop1];
 						});
