f9b308ae6016b23b7dc789c5e685aa85a53c134c
Carl Ruiz <carl.45.cr@gmail.com>
2016-09-05 11:37:04 +0200

    create structure database provinces

--- client/js/module/home/homeModule.js
+++ client/js/module/home/homeModule.js
@@ -11,15 +11,15 @@ angular.module("homeModule", ['zingchart-angularjs'])
 				$scope.rankingFrameworks=data[0]._frameworks;
 				$scope.rankingOtherSkills=data[0]._othersskills;
 
-				$scope.valuesOfFrontEnd = getDataAndRender.getDataRankingLanguages(data);
-				$scope.valuesOfDataBase = getDataAndRender.getDataRankingDb(data);
-				$scope.valuesOfFramework = getDataAndRender.getDataRankingFrameworks(data);
-				$scope.valuesOfOther = getDataAndRender.getDataRankingOtherSkills(data);
+				$scope.valuesOfFrontEnd = getDataAndRender.getDataRanking(data[0]._fivePosLang);
+				$scope.valuesOfDataBase = getDataAndRender.getDataRanking(data[0]._fivePosBack);
+				$scope.valuesOfFramework = getDataAndRender.getDataRanking(data[0]._fivePosFrameworks);
+				$scope.valuesOfOther = getDataAndRender.getDataRanking(data[0]._fivePosOtherSkills);
 
-				$scope.dataConfig = createObjectChart.crateChartForLanguages(data);
-				$scope.dataConfigDb = createObjectChart.crateChartForBackEnd(data);
-				$scope.dataOther = createObjectChart.crateChartForOtherSkills(data);
-				$scope.dataFrameworks = createObjectChart.crateChartForFrameworks(data);
+				$scope.dataConfig = createObjectChart.crateChart(data[0]._offersLanguage,'Programming Languages');
+				$scope.dataConfigDb = createObjectChart.crateChart(data[0]._offersBackEnd,'Data Base');
+				$scope.dataOther = createObjectChart.crateChart(data[0]._othersskills,'Frameworks');
+				$scope.dataFrameworks = createObjectChart.crateChart(data[0]._frameworks,'Other Skills');
 
 
 
--- client/utils/dataChartAndRender.js
+++ client/utils/dataChartAndRender.js
@@ -1,58 +1,19 @@
 angular.module("myChartCreate")
 	.factory('getDataAndRender', function () {
 
-		function getDataRankingLanguages(data){
-				var pos1=parseFloat(data[0]._fivePosLang.positionOne);
-				var pos2=parseFloat(data[0]._fivePosLang.positionTwo);
-				var pos3=parseFloat(data[0]._fivePosLang.positionThree);
-				var pos4=parseFloat(data[0]._fivePosLang.positionFour);
-				var pos5=parseFloat(data[0]._fivePosLang.positionFive);
+		function getDataRanking(data){
+				var pos1=parseFloat(data.positionOne);
+				var pos2=parseFloat(data.positionTwo);
+				var pos3=parseFloat(data.positionThree);
+				var pos4=parseFloat(data.positionFour);
+				var pos5=parseFloat(data.positionFive);
 
-				valuesOfFrontEnd = [[pos5],[pos4],[pos2],[pos1],[pos3]];
+				values = [[pos1],[pos2],[pos3],[pos4],[pos5]];
 
-			return valuesOfFrontEnd;
-		}
-
-		function getDataRankingDb(data){
-				var pos1Db=parseFloat(data[0]._fivePosBack.positionOne);
-				var pos2Db=parseFloat(data[0]._fivePosBack.positionTwo);
-				var pos3Db=parseFloat(data[0]._fivePosBack.positionThree);
-				var pos4Db=parseFloat(data[0]._fivePosBack.positionFour);
-				var pos5Db=parseFloat(data[0]._fivePosBack.positionFive);
-
-				valuesOfDataBase = [[pos2Db],[pos1Db],[pos3Db],[pos4Db],[pos5Db]];
-
-			return valuesOfDataBase;
-		}
-
-		function getDataRankingFrameworks(data){
-				var pos1Frame=parseFloat(data[0]._fivePosFrameworks.positionOne);
-				var pos2Frame=parseFloat(data[0]._fivePosFrameworks.positionTwo);
-				var pos3Frame=parseFloat(data[0]._fivePosFrameworks.positionThree);
-				var pos4Frame=parseFloat(data[0]._fivePosFrameworks.positionFour);
-				var pos5Frame=parseFloat(data[0]._fivePosFrameworks.positionFive);
-
-				valuesOfFrameworks = [[pos1Frame],[pos2Frame],[pos3Frame],[pos4Frame],[pos5Frame]];
-
-			return valuesOfFrameworks;
-		}
-
-		function getDataRankingOtherSkills(data){
-				var pos1Other=parseFloat(data[0]._fivePosOtherSkills.positionOne);
-				var pos2Other=parseFloat(data[0]._fivePosOtherSkills.positionTwo);
-				var pos3Other=parseFloat(data[0]._fivePosOtherSkills.positionThree);
-				var pos4Other=parseFloat(data[0]._fivePosOtherSkills.positionFour);
-				var pos5Other=parseFloat(data[0]._fivePosOtherSkills.positionFive);
-
-				valuesOfOther = [[pos1Other],[pos2Other],[pos3Other],[pos4Other],[pos5Other]];
-
-			return valuesOfOther;
+			return values;
 		}
 
 		return{
-			getDataRankingLanguages : getDataRankingLanguages,
-			getDataRankingDb : getDataRankingDb,
-			getDataRankingFrameworks : getDataRankingFrameworks,
-			getDataRankingOtherSkills : getDataRankingOtherSkills
+			getDataRanking : getDataRanking
 		};
 });
